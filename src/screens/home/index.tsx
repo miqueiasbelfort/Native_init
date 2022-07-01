edfc6c679102bbade40c4e0afa781bb5e5c840f1
miqueiasbelfort <kawasousa1554@gmail.com>
2022-07-01 11:25:51 -0300

    first-commit

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,14 @@
+import { Text } from 'react-native'
+import React from 'react'
+
+import { Container } from './styles'
+
+const Home = () => {
+  return (
+    <Container>
+      <Text>Home</Text>
+    </Container>
+  )
+}
+
+export default Home
